272b2569332958a1afe510f431bfb96396877319
adityaupd <cyber02adi@gmail.com>
2025-09-07 15:25:07 +0530

    here u go

--- Java_Output_Formating.java
+++ Java_Output_Formating.java
@@ -1,6 +1,17 @@
-public class Java_Output_Formating {
+import java.util.Scanner;
 
+public class Java_Output_Formating {
     public static void main(String[] args) {
-        System.out.println("helloworld.");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("================================");
+        for (int i = 0; i < 3; i++) {
+            String word = sc.next();
+            int number = sc.nextInt();
+            System.out.printf("%-15s%03d%n", word, number);
+        }
+        System.out.println("================================");
+
+        sc.close();
     }
 }
